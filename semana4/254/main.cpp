dda86de41f6045ec1c4022afadab4aef63e1284c
Lalo <eedmolteen@gmail.com>
2017-10-10 12:26:33 -0300

    intento

--- semana4/254/main.cpp
+++ semana4/254/main.cpp
@@ -1,58 +1,83 @@
-#include <stdio.h>
-#include <string.h>
-char s[128] = {'\0'};
-int n, b[128] = {0}, lb = 0;
-void Transform(int len)
-{
-    int a[128] = {0}, la = 0; lb = 0;
-    for (int i = 0; i < len; i++) a[i] = s[len - 1 - i] - '0';
-    while (len)
-    {
+#include <bitset>
+#include <iostream>
+
+using namespace std;
+
+void swap(int * a, int * b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int string_number_to_reverse_binary(string string_number, int binary[]){
+    int a[128] = {0};
+    int binary_size = 0;
+    int string_number_size = string_number.size();
+    for (int i = 0; i < string_number_size ; i++) {
+        a[i] = string_number[string_number_size - 1 - i] - '0';
+        cout << "a[" << i << "] = " << a[i] << endl;
+    }
+    while (string_number_size){
         int mod = 0, mul;
-        for (int i = len - 1; i >= 0; i--)
+        for (int i = string_number_size - 1; i >= 0; i--)
         {
             mul = mod * 10 + a[i];
             a[i] = mul >> 1;
             mod = mul & 1;
         }
-        b[lb++] = mod;
-        if (!a[len - 1]) len--;
+        if(mod){
+            binary[binary_size] = 1;
+        }
+        else{
+            binary[binary_size] = 0;
+        }
+        cout << binary[binary_size] << " ";
+        binary_size++;
+        if (!a[string_number_size - 1]) {
+            string_number_size--;
+        }
     }
+    cout << endl;
+    return binary_size;
 }
-int main()
-{
-    int tower1 = 0, tower2 = 0, tower3 = 0, tmp;
-    while (scanf("%d %s", &n, s) == 2)
-    {
-        if (!n && s[0] == '0') break;
-        Transform(strlen(s));
-        tower1 = 0; tower2 = 0; tower3 = 0;
-        printf("%d\n",lb);
-        for (int i = 0; i < lb; i++)
-        {
 
-            if (b[i])
-            {
-                printf("%d 3 5",b[i]);
-                tower3++;
-                tmp = tower1; tower1 = tower2; tower2 = tmp;
+int main(){
+    int n, T1, T2, T3;;
+    string m;
+    cin >> n >> m;
+    int binary[100]; //suficiente para representar 2^100-1, cada celda es 1 o 0
+    while ( n!=0 and m != "0"){
+        int binary_size = string_number_to_reverse_binary(m, binary);
+        T1 = 0; T2 = 0; T3 = 0;
+        for (int i = binary_size-1; i >= 0; i--) {
+            printf("%c ", binary[i]);
+            int bit = binary[i];
+            if (bit == 1){
+                T3++;
+                swap(&T1, &T2);
+//                tmp = T1; T1 = T2; T2 = tmp;
             }
-            else
-            {
-                tower1++;
-                tmp = tower2; tower2 = tower3; tower3 = tmp;
+            else{
+                T1++;
+                swap(&T2, &T3);
+                //tmp = T2; T2 = T3; T3 = tmp;
             }
         }
-        int left = n - lb;
-        if (left & 1)
-        {
-            tmp = tower2;
-            tower2 = tower3;
-            tower3 = tmp;
+        int left = n - binary_size;
+        if (left & 1){
+            swap(&T2, &T3);
+           /* tmp = T2;
+            T2 = T3;
+            T3 = tmp;*/
         }
-        tower1 += left;
-        if (n & 1) printf("%d %d %d\n", tower1, tower3, tower2);
-        else printf("%d %d %d\n", tower1, tower2, tower3);
+        T1 += left;
+        if (n & 1){
+            printf("%d %d %d\n", T1, T3, T2);
+        }
+        else {
+            printf("%d %d %d\n", T1, T2, T3);
+        }
+        cin >> n >> m;
     }
     return 0;
 }
