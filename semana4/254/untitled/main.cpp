d500e2f14934b141e3a1751be118d826498ac1ef
Lalo <eedmolteen@gmail.com>
2017-10-14 15:43:34 -0300

    763 en python 3 presentation error. ignorar la version en C++

--- semana4/254/untitled/main.cpp
+++ semana4/254/untitled/main.cpp
@@ -15,27 +15,19 @@ void showstack(pila gq){
 }
 
 int main() {
-
     int n, steps, torre_actual, disk, torre_menor, torre_mayor, torre_menor_pos, torre_mayor_pos;
-
-    pila torre[3];
-
     cin >> n >> steps;
-
     while(n != 0 && steps != 0) {
+        pila torre[3];
         torre[0].push(101);
         torre[1].push(101);
         torre[2].push(101);
         for (int i = n; i > 0; i--) {
             torre[0].push(i);
         }
-
-
         for(int j = 0; j < steps; j++){
             if(j%2 == 0){
-
                 torre_actual = (j*2)%3;
-
                 disk = torre[torre_actual].top();
                 torre[torre_actual].pop();
                 torre[(torre_actual + 1)%3].push(disk);
@@ -44,30 +36,24 @@ int main() {
                 torre_menor = n;
                 torre_mayor = 2;
                 for(int k = 0; k < 3; k++){
-                    if(torre[k].top() != 1){
-                        if(torre[k].top() < torre_menor){
-                            torre_menor = torre[k].top();
+                    int top_disk = torre[k].top();
+                    if( top_disk != 1){
+                        if(top_disk < torre_menor){
+                            torre_menor = top_disk;
                             torre_menor_pos = k;
                         }
-                        if(torre[k].top() > torre_mayor){
-                            torre_mayor = torre[k].top();
+                        if(top_disk > torre_mayor){
+                            torre_mayor = top_disk;
                             torre_mayor_pos = k;
                         }
                     }
                 }
-
                 torre[torre_menor_pos].pop();
                 torre[torre_mayor_pos].push(torre_menor);
             }
         }
-
         cout << torre[0].size()-1 <<" "<< torre[1].size()-1 <<" "<< torre[2].size()-1 << endl;
-
-        torre[0]=stack<int>();
-        torre[1]=stack<int>();
-        torre[2]=stack<int>();
         cin >> n >> steps;
     }
-
     return 0;
 }
