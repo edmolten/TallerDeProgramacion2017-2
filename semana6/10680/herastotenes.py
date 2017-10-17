9338d75d2b9b52514e4768696f06a8bc914a5fa5
Lalo <eedmolteen@gmail.com>
2017-10-17 14:58:25 -0300

    herastotenes

---
+++ semana6/10680/herastotenes.py
@@ -0,0 +1,39 @@
+'''
+void
+sieve()
+{
+    bool
+isComposite[MAX] = {false};
+for (int
+i = 2;
+i * i <= MAX;
+i + +)
+{
+if (isComposite[i] == false)
+for (int j=2; j * i <= MAX; j++)
+isComposite[i * j] = true;
+}
+
+// Store all prime numbers in vector primes[]
+for (int i=2; i <= MAX; i++)
+if (isComposite[i] == false)
+primes.push_back(i);
+}
+'''
+
+
+def hera(n):
+    primes = []
+    composites = [False]*n
+    for i in range(2, n):
+        j = 2
+        while j * i < n:
+            composites[i * j] = True
+            j += 1
+    for i in range(2, n):
+        if not composites[i]:
+            primes.append(i)
+    return primes
+
+p = hera(17)
+print(p)
