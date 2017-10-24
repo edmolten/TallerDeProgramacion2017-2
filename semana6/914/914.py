817dccf6696a7d3f6953e084652a40f041d55988
Yerson Andres Escobar Maturana <yescobar@alumnos.inf.utfsm.cl>
2017-10-24 00:17:13 -0300

    Terminado el problema, no me lo pesca uva pero acepta todos los putos outputs de udebug

--- semana6/914/914.py
+++ semana6/914/914.py
@@ -15,48 +15,53 @@ def hera(n):
 
 primos = hera(1000000)
 
-def binary_search(numero, arreglo, low, upp):
-    mid = (upp + low) // 2
+def binary_search(numero, arreglo):
+    lo = 0
+    up = len(arreglo) - 1
 
-    print(upp, low)
-    
-    if low > upp:
-        if numero > arreglo[low]:
-            return low
-        elif numero < arreglo[upp]:
-            return upp
-    elif numero == arreglo[mid]:
-        return mid
-    elif numero < arreglo[mid]:
-        return binary_search(numero, arreglo, low, mid - 1)
-    else:
-        return binary_search(numero, arreglo, mid + 1, upp)
+    while lo < up:
+        mid = (lo + up) // 2
+
+        if primos[mid] > numero:
+            up = mid
+        elif primos[mid] < numero:
+            lo = mid + 1
+        else:
+            return mid;
+
+    return lo
 
 T = input()
 
 for cases in range(int(T)):
     L, U = input().strip().split()
 
-    lower = binary_search(int(L), primos, 0, len(primos) - 1)
-    upper = binary_search(int(U), primos, 0, len(primos) - 1)
-    
-    while primos[lower + 1] < int(L):
-        lower += 1
+    lower = binary_search(int(L), primos)
+    upper = binary_search(int(U), primos)
 
-    while primos[upper - 1] > int(U):
-        upper -= 1
-
-    diffs = [t - s for s,t in zip(primos[lower:upper],primos[lower+1:upper])]
-
+    if upper - lower == 1:
+        if primos[upper] > int(U):
+            diffs = list()
+        else:
+            diffs = [primos[upper] - primos[lower]]
+    else:
+        diffs = [t - s for s,t in zip(primos[lower:upper],primos[lower+1:upper])]
     count = Counter(diffs)
 
-    max1 = count.most_common()[0][1]
-    max2 = 0
-    if(len(count.most_common()) > 1): max2 = count.most_common()[1][1]
-    
-    if max2 == 0:
-        print("The jumping champion is " + str(max1))
-    elif max1 > max2:
-        print("The jumping champion is " + str(max1))
+    if(len(count) > 0):
+        max1 = count.most_common()[0][1]
+        max2 = 0
+        if(len(count.most_common(2)) > 1): max2 = count.most_common(2)[1][1]
+        jumping_champion = count.most_common()[0][0]
+
+        if max2 == 0:
+            print("The jumping champion is " + str(jumping_champion))
+        elif max1 == max2:
+            print("No jumping champion")
+        else:
+            print("The jumping champion is " + str(jumping_champion))
     else:
         print("No jumping champion")
+
+print("")
+
