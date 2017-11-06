d729ba73a562dc1dbc76f3a085183dd7cd633436
Cristóbal Galleguillos <c.gallegu@hotmail.com>
2017-11-06 14:28:23 -0300

    asd

--- semana8/494B/494B.py
+++ semana8/494B/494B.py
@@ -8,6 +8,7 @@ k = -1
 par = [0]* (100005)
 f = [0]* (100005)
 tot = [0]* (100005)
+sum1 = [0]* (100005)
 g = [0]* (100005)
 par[0] = -1
 
@@ -31,7 +32,7 @@ for i in range(1,N): #podria partir de 0
         g[i]=1
 
 f[0] = 0
-sum[0] = 0
+sum1[0] = 0
 tot[0] = 0
 ans = 0
 
@@ -39,14 +40,14 @@ for i in range(2,N+1): #podría ser desde 2
     if g[i-1]:
         f[i] = tot[i - M] + i - M + 1
         f[i] %= MOD
-        sum[i] = f[i] + sum[i - 1]
-        sum[i] %= MOD
+        sum1[i] = f[i] + sum1[i - 1]
+        sum1[i] %= MOD
     else:
         f[i] = f[i - 1]
-        sum[i] = f[i] + sum[i - 1]
-        sum[i] %= MOD
+        sum1[i] = f[i] + sum1[i - 1]
+        sum1[i] %= MOD
 
-    tot[i] = sum[i] + tot[i - 1]
+    tot[i] = sum1[i] + tot[i - 1]
     tot[i] %= MOD
     ans += f[i]
     ans %= MOD
