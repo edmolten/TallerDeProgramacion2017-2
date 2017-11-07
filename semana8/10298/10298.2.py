a2f0fbc07c0190bb6d618dc07ab647263ff1b085
Cristóbal Galleguillos <c.gallegu@hotmail.com>
2017-11-07 15:38:12 -0300

    cambio de nombres para mandar problemas semana 8 y creacion semana 9

--- semana8/10298/10298.2.py
+++ semana8/10298/10298.2.py
@@ -1,3 +1,5 @@
+
+'''
 word = input()
 
 def hashing(word):
@@ -28,4 +30,18 @@ while(word!="."):
                 print(len(word)//i)
                 break
 
+    word = input()
+'''
+
+word = input()
+while word != ".":
+    sublen = 1
+    for i in range(1, len(word)):
+        while word[i] != word[i % sublen]:
+            sublen += 1
+    i += 1
+    if i % sublen == 0:
+        print(i // sublen)
+    else:
+        print(1)
     word = input()
