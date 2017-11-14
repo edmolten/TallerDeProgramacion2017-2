34f28e2bd15b48ead73070efdbdfaed97bed6bea
Lalo <eedmolteen@gmail.com>
2017-11-13 21:21:18 -0300

    semana9 625b accepted

--- semana9/625b/main.cpp
+++ semana9/625b/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 #define L 100000
 
-int main(){
+int main() {
     // static for memory issues in Windows
     static int prefix_table[L], j, s_len, t_len, i;
     static long long result;
@@ -18,11 +18,11 @@ int main(){
     t_len = t.size();
 
     // Build KMP prefix table ---------------------------
-    for(i=1; i<t_len; i++){
-        while(j>=0 && t[j+1] != t[i]) {
+    for (i = 1; i < t_len; i++) {
+        while (j >= 0 && t[j + 1] != t[i]) {
             j = prefix_table[j];
         }
-        if(t[j+1] == t[i]){
+        if (t[j + 1] == t[i]) {
             j++;
         }
         prefix_table[i] = j;
@@ -30,22 +30,24 @@ int main(){
 
     // Run KMP ------------------------------------------
     j = -1;
-    for(i=0; i<s_len; i++){
-        if(t[j+1] == s[i]){
+    for (i = 0; i < s_len; i++) {
+        if (t[j + 1] == s[i]) {
             j++;
-        }
-        else {
-            while (j >= 0 && t[j + 1] != s[i]){
+        } else {
+            while (j >= 0 && t[j + 1] != s[i]) {
                 j = prefix_table[j];
             }
-            if(t[j+1] == s[i]){
+            if (t[j + 1] == s[i]) {
                 j++;
             }
         }
-        if(j == t_len-1) {
+        if (j == t_len - 1) {
             result += 1;
+            j =-1;
+
         }
     }
 
     cout << result << endl;
     return 0;
+}
