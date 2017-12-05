bbd55f3d06b40ce0799b420701e70fe201c08a5c
Lalo <eedmolteen@gmail.com>
2017-12-05 15:02:33 -0300

    das

---
+++ semana13/10577/src/cly/Main.java
@@ -0,0 +1,12 @@
+package cly;
+
+public class Main {
+
+    double getA(double x, double y, double z){
+        double f1 = x1*()
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
