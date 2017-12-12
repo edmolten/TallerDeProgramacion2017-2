8829dba2e5b88003813e3e2c452883caeecdb2cd
Cristóbal Galleguillos <c.gallegu@hotmail.com>
2017-12-12 14:58:36 -0300

    asdadasdsa

---
+++ semana14/471d/main.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    int n,w;
+    cin >> n >> w;
+    vector <int> bear;
+    vector <int> elephant;
+    string b, e;
+    while(cin){
+        for(int i = 0;  ){
+
+        }
+    }
+    return 0;
+}
